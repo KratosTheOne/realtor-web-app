440f1721c062d214effd61bfd8fc936db495a2e5
Kaustubh <kaustubhsinha2001@gmail.com>
2023-06-20 02:06:01 +0530

    Create listing UI with OnChange and OnSubmit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Header from './components/Header';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
 
 
 function App() {
@@ -46,6 +47,10 @@ function App() {
           <Route 
             path="/forgot-password"
             element={<ForgotPassword/>}
+          />
+          <Route 
+            path="/create-listing"
+            element={<CreateListing/>}
           />
         </Routes>  
       </BrowserRouter>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
 import { doc, updateDoc } from "firebase/firestore"
 import { db } from '../firebase.config';
+import { FcHome } from 'react-icons/fc';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
 
@@ -90,6 +92,12 @@ const Profile = () => {
               <p onClick={onLogOut} className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer">Sign Out</p>
             </div>
           </form>
+          <button type="submit" className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded-md shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800">
+            <Link to="/create-listing" className="flex justify-center items-center">
+              <FcHome className="mr-2 text-3xl bg-red-200 rounded-full p-1 border-2" />
+              Sell or rent your property
+            </Link>
+          </button>
         </div>
       </section>
     </>
